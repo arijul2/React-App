819be1979198d99418fa0290314fa0dd8d8bf148
Ari Juljulian <arijul2@me.com>
2025-09-15 13:48:29 -0700

    using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["New York", "Madrid", "Lisbon", "London", "Paris"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
